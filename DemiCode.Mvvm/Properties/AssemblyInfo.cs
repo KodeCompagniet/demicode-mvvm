8bf4acd0135f3cf3b048d2d1ab40db3f8e5a220e
Peter Lillevold <plillevold@gmail.com>
2019-02-27 13:05:30 +0100

    Fixed signing (InternalsVisibleTo in signed assembly requires reference to signed assemblies) to shared snk

--- DemiCode.Mvvm/Properties/AssemblyInfo.cs
+++ DemiCode.Mvvm/Properties/AssemblyInfo.cs
@@ -13,4 +13,4 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("c5ae82f7-b437-41b8-8435-ce4a12a919fe")]
 
-[assembly: InternalsVisibleTo("DemiCode.Mvvm.Test")]
+[assembly: InternalsVisibleTo("DemiCode.Mvvm.Test, PublicKey=0024000004800000940000000602000000240000525341310004000001000100f5fb3f7a8615b685a5485059e5753a1ccfbaf98aeaf70e998cc7a736da553bb4aa66fc96864e5f833eef6eabc42a5b277db1180f58c0325988f752a86cf829c8d2a6a83ea4320fe039313cbb17bb4e08f8384c6d6c42c26edaaeb78f94736258c10f99c36be77339e9fcf0732a5f67265a01e0c9c58970fd55c0c58630860ec6")]
